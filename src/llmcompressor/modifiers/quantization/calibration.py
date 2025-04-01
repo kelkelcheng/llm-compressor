6cae23ff6eaa6571963ec292f7c64929935e6da0
Dipika Sikka <dipikasikka1@gmail.com>
2025-04-01 17:47:46 +0000

    update

--- src/llmcompressor/modifiers/quantization/calibration.py
+++ src/llmcompressor/modifiers/quantization/calibration.py
@@ -57,12 +57,11 @@ def initialize_observer(
     quantization_args = getattr(quantization_scheme, arg_name, None)
     # dont need observers for dynamic
     if quantization_args is not None and not quantization_args.dynamic:
-        global_scale = getattr(module, "global_scale", None)
+        global_scale = getattr(module, f"{base_name}_global_scale", None)
         if global_scale is not None:
             assert base_name == "weight"
             assert quantization_args.num_bits == 4
             assert quantization_args.type == QuantizationType.FLOAT
-
         observer = Observer.load_from_registry(
             quantization_args.observer,
             quantization_args=quantization_args,
@@ -94,6 +93,7 @@ def call_observer(module: Module, base_name: str, value: Optional[torch.Tensor] 
         raise ValueError("Must provide a value to observe if not using weight observer")
 
     observer = getattr(module, f"{base_name}_observer")
+    breakpoint()
     updated_scale, updated_zero_point = observer(value, g_idx=g_idx)
 
     # update scale and zero point
@@ -133,6 +133,7 @@ def update_weight_zp_scale(module: Module):
 
     if module.quantization_scheme.weights is not None:
         # set weight scale and zero_point up front, calibration data doesn't affect it
+        breakpoint()
         call_observer(module=module, base_name="weight")
 
 
